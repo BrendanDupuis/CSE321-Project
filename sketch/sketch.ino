5dbc1a8a52877ee477bb3dccceff9aa48e3d7461
Enzo Mclauchlin <enzomcla@buffalo.edu>
2025-11-07 12:17:02 -0500

    LCD display showing rpm, rpm calculated

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal_I2C.h>
+
 #define s0 8
 #define s1 9
 #define s2 10
@@ -7,9 +9,15 @@
 volatile int  Red=0, Blue=0, Green=0;
 volatile unsigned long prevTime = 0, period = 0;
 volatile float rpm = 0;
+bool sensorTrigger = false;
+volatile unsigned long prev;
+
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() 
 {
+  lcd.init(); // initialize the lcd
    pinMode(s0,OUTPUT);
    pinMode(s1,OUTPUT);
    pinMode(s2,OUTPUT);
@@ -17,19 +25,34 @@ void setup()
    pinMode(out,INPUT);
 
    Serial.begin(9600);
+
+   lcd.backlight();
+   lcd.setCursor(0, 0);
    
+
    digitalWrite(s0,HIGH); //Putting S0/S1 on HIGH/HIGH levels  means the output frequency scalling is at 100% (recommended)
    digitalWrite(s1,HIGH);  //LOW/LOW is off HIGH/LOW is 20% and LOW/HIGH is  2%
+
+
 }
 
 void loop(){
+  if(sensorTrigger && prevTime + 500 < millis()){
+    sensorTrigger = false;
+  }
+
   GetColors();
-  if (Red<Blue && Red<=Green && Red<23){
+  if (Blue<Red && Blue<Green && Blue<14 && !sensorTrigger){
+      sensorTrigger = true;
       unsigned long currentTime = millis();
       period = currentTime-prevTime;
       prevTime = currentTime;
       rpm = 60000.0/period;
-      Serial.println(rpm);
+      lcd.setCursor(0, 0);
+      lcd.print("rpm:");
+      lcd.setCursor(0,1);
+      lcd.print(rpm);
+      Serial.print(rpm);
   }
 }
 
